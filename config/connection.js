5dcd1aba80fac35d63a5a486288d612dd503a812
Mickey S <marimikaels@gmail.com>
2017-10-06 21:34:59 -0400

    connection change

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,3 @@
-// var mysql = require("mysql");
-
-
-// if (process.env.JAWSDB_URL) {
-// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "Iliketrains4",
-//   database: "burgers_db"
-// });
-// };
-
-
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// module.exports = connection;
-
 var mysql = require("mysql");
 
 
@@ -37,14 +12,39 @@ var connection = mysql.createConnection({
 });
 };
 
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-
   console.log("connected as id " + connection.threadId);
-
 });
 
 module.exports = connection;
+
+// var mysql = require("mysql");
+
+
+// if (process.env.JAWSDB_URL) {
+// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Iliketrains4",
+//   database: "burgers_db"
+// });
+// };
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+
+//   console.log("connected as id " + connection.threadId);
+
+// });
+
+// module.exports = connection;
