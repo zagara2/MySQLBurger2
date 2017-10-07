b537a68e212b9750b0a68a3630dfae55a83d0430
Mickey S <marimikaels@gmail.com>
2017-10-06 21:12:10 -0400

    server change

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,28 @@
+// var mysql = require("mysql");
+
+
+// if (process.env.JAWSDB_URL) {
+// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Iliketrains4",
+//   database: "burgers_db"
+// });
+// };
+
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// module.exports = connection;
+
 var mysql = require("mysql");
 
 
@@ -12,13 +37,14 @@ var connection = mysql.createConnection({
 });
 };
 
-
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
+
   console.log("connected as id " + connection.threadId);
+
 });
 
 module.exports = connection;
