f30729d601df002c477160f788dc561afef0d370
Mickey S <marimikaels@gmail.com>
2017-10-06 21:39:30 -0400

    trying to make heroku work

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,3 @@
-var mysql = require("mysql");
-
-
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Iliketrains4",
-  database: "burgers_db"
-});
-};
-
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-module.exports = connection;
-
 // var mysql = require("mysql");
 
 
@@ -37,14 +12,26 @@ module.exports = connection;
 // });
 // };
 
+
 // connection.connect(function(err) {
 //   if (err) {
 //     console.error("error connecting: " + err.stack);
 //     return;
 //   }
-
 //   console.log("connected as id " + connection.threadId);
-
 // });
 
 // module.exports = connection;
+
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
